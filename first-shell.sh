eb449105a484575772fe254e82f82f0bd97fbfe3
Tejaswini-Nandhu <nandhu@tejas.com>
2024-06-14 09:20:16 +0530

    dumped basic shell scripts

---
+++ first-shell.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "This is the first shell scripting file"
+#echo is used to print the text
+#lets create a automated script to create files
+touch automated-file1 automated-file2
+
+#whenever i run this file two files will be created with the names automated-file1,2
---
+++ second-shell.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#a script to create a directory with a file
+mkdir automated-dir
+touch file-ad
+
+#using this script we can only create a directory and a file but not a directory with a file in it
